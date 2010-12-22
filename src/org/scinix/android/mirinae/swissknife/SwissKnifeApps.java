f66251150db063462d0506761f9b339924dd1d7a
Yonghwan SO <sio4@users.sf.net>
2010-12-23 00:35:17 +0900

    basic structure for Apps tab.

--- src/org/scinix/android/mirinae/swissknife/SwissKnifeApps.java
+++ src/org/scinix/android/mirinae/swissknife/SwissKnifeApps.java
@@ -1,16 +1,18 @@
 package org.scinix.android.mirinae.swissknife;
 
-import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class SwissKnifeApps extends Activity {
+public class SwissKnifeApps extends ListActivity {
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -19,21 +21,53 @@ public class SwissKnifeApps extends Activity {
 
 		ArrayList<String> appArrayList = new ArrayList<String>();
 
+		PackageManager pm = getPackageManager();
 		/* get apps list from directory. but we need XML for it maybe. */
-		File appDir = new File("/system/app");
-		if (appDir.exists() && appDir.isDirectory()) {
-			ArrayList<File> appFileArrayList = new ArrayList<File>(Arrays.asList(appDir.listFiles()));
-			Iterator<File> e = appFileArrayList.iterator();
-			while (e.hasNext()) {
-				appArrayList.add((e.next()).getName());
+		List<ApplicationInfo> ais = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+		Toast.makeText(this, "loading " + ais.size() + " apps...", Toast.LENGTH_LONG).show();
+		Iterator<ApplicationInfo> e = ais.iterator();
+		while (e.hasNext()) {
+			ApplicationInfo ai = e.next();
+			String val = (String) ai.loadLabel(pm) + "\n" + ai.sourceDir;
+			if (val != null) {
+				appArrayList.add(val);
 			}
 		}
-		ListView listView = (ListView) findViewById(R.id.apps_list);
-		listView.setAdapter(new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_multiple_choice,
-				appArrayList));
 
+		setListAdapter(new SwissKnifeAppAdapter(this, R.layout.apps_row, ais));
+		final ListView listView = getListView();
 		listView.setItemsCanFocus(false);
 		listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+		//		R.layout.apps_row, R.id.ctv, appArrayList));
+		//listView.setItemsCanFocus(false);
+		//listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+		//listView.setOnItemClickListener(this);
+		//listView.setOnItemLongClickListener(this);
+		//listView.setOnItemSelectedListener(this);
 	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		// Get the item that was clicked
+		Toast.makeText(this, "" + l.getCheckItemIds()[0], Toast.LENGTH_LONG).show();
+		ApplicationInfo ai = (ApplicationInfo) this.getListAdapter().getItem(position);
+		Toast.makeText(this, "Package Name: " + ai.packageName, Toast.LENGTH_LONG).show();
+	}
+
+	protected void onListItemSelected(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		// Get the item that was clicked
+		Object o = this.getListAdapter().getItem(position);
+		String keyword = o.toString();
+		Toast.makeText(this, "You selected: " + keyword,
+				Toast.LENGTH_SHORT).show();
+
+		ApplicationInfo ai = (ApplicationInfo) this.getListAdapter().getItem(position);
+		Toast.makeText(this, "Package Name: " + ai.packageName, Toast.LENGTH_LONG).show();
+	}
+
+
 }
